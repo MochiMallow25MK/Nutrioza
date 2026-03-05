58121c2039a75e638ccc452139c153e29bd3f6b5
Mohamed Kazan <Mohamed.Kazan24d@eslsca.edu.eg>
2026-03-05 21:38:53 +0200

    Removed useless files !

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_URL', 'http://localhost/nutrioza/');
-define('APP_NAME', 'Nutrioza');
-define('UPLOAD_PATH', $_SERVER['DOCUMENT_ROOT'] . '/nutrioza/assets/uploads/');
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
--- interfaces/ReportInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-interface ReportInterface {
-    public function generate($type, $filters);
-    public function export($format, $data);
-}
-?>
